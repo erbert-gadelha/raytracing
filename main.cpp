8f0d878d3e3e68ca7a1b8f5773a7ddf450e4b613
erbert-gadelha <ebgr@cin.ufpe.br>
2024-05-25 01:27:35 -0300

    definicao de um novo cenario

--- main.cpp
+++ main.cpp
@@ -2,68 +2,76 @@
 #include <vector>
 #include <tuple>
 
-
-/*#include "Object.h"
+#include "Object.h"
 #include "Sphere.h"
-#include "Plane.h"*/
+#include "Plane.h"
 #include "Mesh.h"
 #include "Vector3.h"
-
-/*#include "Camera.h"
+#include "Camera.h"
 #include "Vector3.h"
 
-#include "FileWriter.h"*/
+#include "FileWriter.h"
 
 
 using namespace std;
 
+Object* CreateCube() {
+    std::vector<Vector3> vertices = {
+        {0, 0, 0}, {1, 0, 0}, {1, 1, 0}, {0, 1, 0},
+        {0, 0, 1}, {1, 0, 1}, {1, 1, 1}, {0, 1, 1}
+    };
 
-int main() {
-    vector<Vector3> vertices = {
-        Vector3(1,2,3),
-        Vector3(-1,1,4),
-        Vector3(1,-3,5),
-        Vector3(4,5,0)
-        };
-    vector<tuple<int, int, int>> faces = {{0,1,2},{1,2,3}};
-    Mesh m = Mesh(vertices, faces);
+    std::vector<std::tuple<int, int, int>> faces = {
+        {0, 1, 2}, {2, 3, 0},{4, 5, 6}, {6, 7, 4}, 
+        {0, 1, 5}, {5, 4, 0},{2, 3, 7}, {7, 6, 2},
+        {1, 2, 6}, {6, 5, 1},{0, 3, 7}, {7, 4, 0} 
+    };
 
-    cout << m.to_string() << endl;
-    cout << "wasd" << endl;
+    return new Mesh(vertices, faces);
+}
 
-
-
-
-    /*colorRGB RED   = {255,0,0};
+void Scene_1() {
+    colorRGB RED   = {255,0,0};
     colorRGB GREEN = {0,255,0};
     colorRGB BLUE  = {0,0,255};
     colorRGB YELLOW  = {255,255,0};
-
 
     //Camera camera = Camera(1080, 1080, 1000);
     Camera camera = Camera(512, 512, 1000);
     //Camera camera = Camera(32, 32, 100);
     camera.transform.position.setZ(-10);
 
-    Object* sphere1 = new Sphere(Vector3().ONE*2, Vector3(0,0,0));
-    Object* sphere2 = new Sphere(Vector3().ONE*1, Vector3(1,0.5,0));
-    Object* plane1 = new Plane(Vector3().ONE, Vector3(3,0,0), Vector3(5,0,45));
-    Object* plane2 = new Plane(Vector3().ONE, Vector3(3,-3,0), Vector3(5,0,0));
-    sphere1->color = RED;
-    sphere2->color = BLUE;
-    plane1->color = YELLOW;
-    plane2->color = GREEN;
+
+
+
+    Object* sphere = new Sphere(Vector3().ONE*1, Vector3(2,1.5,0.25));
+    Object* mesh = new Mesh({Vector3(0,2,0),Vector3(0,0,0),Vector3(2,0,0),Vector3(2,2,0)}, {{1,2,3}});
+    Object* plane = new Plane(Vector3().ONE, Vector3(3,-1,0), Vector3(0,0,0));
+    Object* cube = CreateCube();
+    sphere->color = BLUE;
+    plane->color = GREEN;
+    mesh->color = YELLOW;
+    cube->color = RED;
 
     vector<Object*> objects;
-    objects.push_back(sphere1);
-    objects.push_back(sphere2);
-    objects.push_back(plane1);
-    objects.push_back(plane2);
-
+    objects.push_back(sphere);
+    objects.push_back(plane);
+    objects.push_back(mesh);
+    objects.push_back(cube);
 
     string image_ppm = camera.render(objects);
-    FileWriter::saveAsImage(image_ppm);*/
+    FileWriter::saveAsImage(image_ppm);
+}
 
+void Scene_2() {
+    Mesh mesh = Mesh({Vector3(3,0,0),Vector3(0,0,0),Vector3(0,3,0),Vector3(2,2,0)}, {{0,1,2}});
+    mesh.cast(Ray(Vector3(0,0,-10), Vector3(0.01,0,0.9)));
+}
+
+int main() {
+    Scene_1();
+    //Scene_2();
+    
     std::cout << "\n===============================\n" << std::endl;
     return 0;
 }
